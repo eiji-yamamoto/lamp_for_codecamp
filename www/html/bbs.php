666e53f2389fc5146d45f57237dab15805c2658c
eiji-yamamoto <eiji_yamamoto@yrglm.co.jp>
2020-05-07 17:35:02 +0900

    Submit bbs.php(first task)

--- www/html/bbs.php
+++ www/html/bbs.php
@@ -1,3 +1,42 @@
+<?php
+$filename = "bbs_log.txt";
+$errors = [];
+
+if ((isset($_POST['name']) === TRUE) && (isset($_POST['comment']) === TRUE)) {
+    // 名前とコメントのエラー処理
+    if ($_POST['name'] === '') {
+        $errors[] = '名前を入力してください。';
+    }
+
+    if ($_POST['comment'] === '') {
+        $errors[] = 'ひとことを入力してください。';
+    }
+
+    if (mb_strlen($_POST['name']) > 20) {
+        $errors[] = '名前は20文字以内で入力してください。';
+    }
+
+    if (mb_strlen($_POST['comment']) > 100) {
+        $errors[] = 'ひとことは100文字以内で入力してください。';
+    }
+
+    // 名前とコメントの登録処理
+    if (count($errors) === 0) {
+        $data = $_POST['name'] . ': ' .  $_POST['comment'] . ' -' . date('Y/m/d H:i:s') . "\n";
+        file_put_contents($filename, $data, FILE_APPEND);
+    }
+}
+
+// ファイル読み込み処理
+$file_data_array = [];
+if (($file_data_array = file($filename)) !== FALSE) {
+    // 最新のコメントが上に来るようにひっくり返す
+    $file_data_array = array_reverse($file_data_array);
+} else {
+    print 'ファイルが読み込めませんでした。';
+}
+?>
+
 <html lang="ja">
 
 <head>
@@ -7,58 +46,30 @@
 
 <body>
     <h1>ひとこと掲示板</h1>
-    <form action="#" method="post">
+    <form method="post">
         名前 : <input type="text" name="name">
         ひとこと: <input type="text" name="comment">
         <input type="submit">
     </form>
+
+    <?php if (count($errors) !== 0) : ?>
+        <ul>
+            <?php foreach ($errors as $value) : ?>
+                <li>
+                    <?php print $value; ?>
+                </li>
+            <?php endforeach ?>
+        </ul>
+    <?php endif ?>
+
+    <hr>
+
+    <?php if ($file_data_array) : ?>
+        <?php foreach ($file_data_array as $value) : ?>
+            <?php print  htmlspecialchars($value, ENT_QUOTES, 'UTF-8'); ?>
+            <br>
+        <?php endforeach ?>
+    <?php endif ?>
 </body>
 
 </html>
-
-<?php
-$filename = "bbs_log.txt";
-$valid_flag = TRUE;
-
-if ((isset($_POST['name']) === TRUE) && (isset($_POST['comment']) === TRUE)) {
-    // 名前とコメントのエラー処理
-    if ($_POST['name'] === '') {
-        print '名前を入力してください。<br>';
-        $valid_flag = FALSE;
-    }
-
-    if ($_POST['comment'] === '') {
-        print 'ひとことを入力してください。<br>';
-        $valid_flag = FALSE;
-    }
-
-    if (mb_strlen($_POST['name']) > 20) {
-        print '名前は20文字以内で入力してください。<br>';
-        $valid_flag = FALSE;
-    }
-
-    if (mb_strlen($_POST['comment']) > 100) {
-        print 'ひとことは100文字以内で入力してください。<br>';
-        $valid_flag = FALSE;
-    }
-
-    // 名前とコメントの登録処理
-    if ($valid_flag) {
-        $data = $_POST['name'] . ': ' .  $_POST['comment'] . ' -' . date('Y/m/d H:i:s') . "\n";
-        file_put_contents($filename, $data, FILE_APPEND);
-    }
-}
-
-// レイアウト調整
-print '<hr>';
-
-// ファイル表示処理
-if (($file_data_array = file($filename)) !== FALSE) {
-    // 最新のコメントが上に来るようにひっくり返す
-    foreach (array_reverse($file_data_array) as $value) {
-        print htmlspecialchars($value, ENT_QUOTES, 'UTF-8') . '<br>';
-    }
-} else {
-    print 'ファイルが読み込めませんでした。';
-}
-?>
