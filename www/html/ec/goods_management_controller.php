5ec47147a44341244d57dfd50bac8c12bcffda63
eiji-yamamoto <eiji_yamamoto@yrglm.co.jp>
2020-05-22 17:11:27 +0900

    Add ec dir for 3rd task

---
+++ www/html/ec/goods_management_controller.php
@@ -0,0 +1,5 @@
+<?php
+require_once('../../include/conf/const.php');
+require_once('../../include/model/ec_model.php');
+
+include_once('../../include/view/ec_goods_management_view.php');
---
+++ www/include/view/ec_goods_management_view.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>商品管理ページ</title>
+</head>
+
+<body>
+    <h1>商品管理ページ</h1>
+
+</body>
+
+</html>
