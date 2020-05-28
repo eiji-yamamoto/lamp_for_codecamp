5744196b1b2521bf6ada2262e4ea94b831edb500
eiji-yamamoto <eiji_yamamoto@yrglm.co.jp>
2020-05-28 11:37:11 +0900

    Fix logout func to clear cache

--- www/html/ec/logout_controller.php
+++ www/html/ec/logout_controller.php
@@ -2,7 +2,21 @@
 require_once('../../include/conf/ec_const.php');
 
 session_start();
-
+$session_name = session_name();
 $_SESSION = [];
+
+if (isset($_COOKIE[$session_name])) {
+    $params = session_get_cookie_params();
+    setcookie(
+        $session_name,
+        '',
+        time() - 60,
+        $params["path"],
+        $params["domain"],
+        $params["secure"],
+        $params["httponly"]
+    );
+}
+
 session_destroy();
 header("Location: " . $_SERVER['PHP_SELF'] . '/../login_controller.php');
